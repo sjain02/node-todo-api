24d4516d1baf5e675af3cc05bce9b99371e3aa78
Saurabh Jain <jpr.saurabh@gmail.com>
2018-07-30 21:46:27 +0530

    added update

---
+++ mongodb-update.js
@@ -0,0 +1,21 @@
+const {MongoClient, ObjectID}=require('mongodb');
+
+
+MongoClient.connect('mongodb://192.168.33.10:27017/test' ,(err,client)=>{
+    if(err){
+        return console.log("Unable to connect to Database server");
+    }
+    console.log('Connected to MongoDB Server')
+    const db=client.db('test');
+    
+    // db.collection('Todos').findOneAndUpdate({"_id" : ObjectID("5b5f311fd3224632143dcfd8")},{$set:{completed:true}},{returnOriginal:false}).then((result)=>{
+    //     console.log(result);
+    // },(err)=>{
+    //     console.log(err);
+    // });
+    db.collection('appuser').findOneAndUpdate({name:'Saurabh Jain'},{$inc:{age:1}},{returnOriginal:false}).then((result)=>{
+        console.log(JSON.stringify(result,undefined,2));
+    },(err)=>{
+        console.log(err);
+    });
+});
